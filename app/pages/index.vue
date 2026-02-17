d31b44256ac15449e8c3f2784603f46b5c9f2324
unknown <Antalyst>
2026-02-17 14:49:05 +0800

    feat: added navigation scrolling and section IDs

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,3 +6,18 @@
         <FouthSection id="travels" />
     </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'instant' 
+  })
+  if (window.location.hash) {
+    history.replaceState(null, null, window.location.pathname)
+  }
+})
+</script>
